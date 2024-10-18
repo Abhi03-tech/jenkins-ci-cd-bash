5bf634198676aae30eff4c6a02edb978874dc42c
Jenkins Automation Server <jenkins@ip-172-31-1-175.ap-south-1.compute.internal>
2024-10-18 08:49:05 +0000

    new commit

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "Hello from Jenkins CI/CD Pipeline!"
-echo "bash.....script for test purposes only" > abhi.sh
+echo "bash script for test purposes only" > abhi.sh
 cat abhi.sh
-New content
